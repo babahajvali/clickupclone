dd3f40f66f5a79fa5a4ecea707d167fa144b0c6d
Baba <babasvali2121@gmail.com>
2026-03-27 12:45:51 +0530

    add the payment feature

---
+++ task_management/interactors/storage_interface/customer_storage_interface.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+class CustomerStorageInterface(ABC):
+    @abstractmethod
+    def create_customer(self, user_id: str, stripe_customer_id: str) -> dict:
+        pass
+
+    @abstractmethod
+    def get_customer_by_user_id(self, user_id: str) -> Optional[dict]:
+        pass
+
+    @abstractmethod
+    def get_customer_by_stripe_id(self, stripe_customer_id: str) -> Optional[dict]:
+        pass
+
+    @abstractmethod
+    def update_payment_method(self, customer_id: str, payment_method: str) -> dict:
+        pass
